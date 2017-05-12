6ef80a519a88f6e479122839818b86e63981436c
sviat <sviat@localhost.localdomain>
2017-05-12 14:56:26 +0300

    update

--- install_package.sh
+++ install_package.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-yum_package="vim tree wget git unzip net-tools bash-completion telnet nmap docker ansible"
+yum_package="vim tree wget git unzip net-tools bash-completion telnet nmap docker-ce ansible yum-utils"
 apt_package="vim tree wget git unzip net-tools bash-completion telnet nmap"
+
+
+
+
+
 
 function test_install {
     if [[ -f /etc/redhat-release ]]; then
+        # update system
+        yum update
+        # Add repo
+        yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+        #Update cache
+        yum makecache fast
         yum -y install $yum_package
     elif [[ -f /etc/debian_version ]]; then
+        apt update && apt upgrade
         apt install -y $apt_package  
     else
         echo 'unknow distro'
+        exit 1
     fi
 }
 
