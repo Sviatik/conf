f588a19d5273a98f60eb4423cefe1f9ea5147667
sviat <sstetsyak@gmail.com>
2017-10-01 13:24:10 +0300

    update

--- install_package.sh
+++ install_package.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #YUM_PACKAGE="vim tree wget git unzip net-tools bash-completion telnet nmap docker-ce ansible yum-utils"
+
+USER=sviat
 APT_PACKAGE="vim tree wget git unzip net-tools bash-completion telnet nmap tlp tlp-rdw google-chrome-stable openconnect \
     sublime-text remmina remmina-plugin-rdp keepassx docker-engine ansible"
 #skype
@@ -61,8 +63,9 @@ function install_package {
 check_su
 
 #conf terminal
-mkdir -p /home/sviat/.config/xfce4/terminal
-wget https://github.com/sviatik/conf/raw/master/terminalrc -P /home/sviat/.config/xfce4/terminal
+mkdir -p /home/$USER/.config/xfce4/terminal
+wget https://github.com/sviatik/conf/raw/master/terminalrc -P /home/$USER/.config/xfce4/terminal
+
 
 
 docker pull microsoft/azure-cli
@@ -83,7 +86,11 @@ sudo wget -O /usr/share/themes/OSX-Arc-White.tar.gz https://github.com/LinxGem33
 sudo tar zxvf /usr/share/themes/OSX-Arc-White.tar.gz &> /dev/null
 rm -rf /usr/share/themes/OSX-Arc-White.tar.gz
 
-ln -s /tmp/ /home/sviat/tmp
+ln -s /tmp/ /home/$USER/tmp
+
+rm -rf /home/$USER/.bashrc
+wget https://github.com/sviatik/conf/raw/master/.bashrc -P /home/$USER/ 
+
 echo "INFO: For install PAckage control in sublime-text3 pls press Ctr+~"
 echo "import urllib.request,os,hashlib; h = '6f4c264a24d933ce70df5dedcf1dcaee' + 'ebe013ee18cced0ef93d5f746d80ef60'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)"
 
