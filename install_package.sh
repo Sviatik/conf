ead92380ce5b779c79528e3a3db9ffc27fb1e718
sviat <sstetsyak@gamil.com>
2017-10-03 22:46:31 +0300

    update

--- install_package.sh
+++ install_package.sh
@@ -4,7 +4,7 @@
 
 USER=sviat
 APT_PACKAGE="vim tree wget git unzip net-tools bash-completion telnet nmap tlp tlp-rdw google-chrome-stable openconnect \
-    sublime-text remmina remmina-plugin-rdp keepassx docker-engine ansible ssh sshfs virtualbox-5.1"
+    sublime-text remmina remmina-plugin-rdp keepassx docker-engine ansible ssh sshfs virtualbox-5.1 htop"
 #skype
 
 
