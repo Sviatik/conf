1df9a21597640da8aa15ddf4d807ae00c2b98dd6
Sviatoslav Stetsiak <sstetsyak@gmail.com>
2017-12-22 10:47:31 +0200

    update

--- opendisconnect.sh
+++ opendisconnect.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo "Disconnecting..."
-sudo pkill -SIGINT openconnect
-
+ps -aux | grep openconnect | grep -v grep
+if [[ $? == 0 ]] 
+then
+	echo "INF: Disconnecting..."
+	sudo pkill -SIGINT openconnect
+else
+	echo "INFO: VPN not connected"
+fi
 # Remove default gateway route rule when there is already a PPTP connection
 # Uncomment line below if your computer is connected to internet through a PPTP connection
 #ip r | grep ppp0 && ip r | grep default | head -n1 | xargs sudo ip r del
